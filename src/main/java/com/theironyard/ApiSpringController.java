d8744b82fab16341cbb6db7cb8e9a18808961394
oakes <zsoakes@gmail.com>
2015-12-11 10:07:02 -0500

    Add controller

---
+++ src/main/java/com/theironyard/ApiSpringController.java
@@ -0,0 +1,11 @@
+package com.theironyard;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by zach on 12/11/15.
+ */
+@RestController
+public class ApiSpringController {
+    static final String SAMPLE_URL = "http://gturnquist-quoters.cfapps.io/api/random";
+}
