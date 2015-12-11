9848e89fb3edbcb8e8b26a4398921422b93b8720
oakes <zsoakes@gmail.com>
2015-12-11 14:37:52 -0500

    Finish API examples

--- src/main/java/com/theironyard/ApiSpringController.java
+++ src/main/java/com/theironyard/ApiSpringController.java
@@ -1,6 +1,15 @@
 package com.theironyard;
 
+import org.springframework.social.facebook.api.Facebook;
+import org.springframework.social.facebook.api.User;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
 
 /**
  * Created by zach on 12/11/15.
@@ -8,4 +17,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ApiSpringController {
     static final String SAMPLE_URL = "http://gturnquist-quoters.cfapps.io/api/random";
+
+    Facebook facebook;
+
+    @Inject
+    public ApiSpringController(Facebook facebook) {
+        this.facebook = facebook;
+    }
+
+    @RequestMapping("/quote")
+    public HashMap getQuote() {
+        RestTemplate query = new RestTemplate();
+        HashMap result = query.getForObject(SAMPLE_URL, HashMap.class);
+        String type = (String) result.get("type");
+        if (type.equals("success")) {
+            HashMap value = (HashMap) result.get("value");
+            return value;
+        }
+        return result;
+    }
+
+    @RequestMapping("/profile")
+    public User getUser(HttpServletResponse response) throws IOException {
+        try {
+            User user = facebook.userOperations().getUserProfile();
+            return user;
+        } catch (Exception e) {
+            response.sendRedirect("/connect/facebook");
+        }
+
+        return null;
+    }
 }
